069fb0a6c5dbfdd400d8990e16ab47ef9a7c0d7d
Victoria Tsiang <victoria.tsiang@gmail.com>
2026-09-04 09:26:21 -0400

    working xz spin

--- Cube.java
+++ Cube.java
@@ -8,7 +8,7 @@ public class Cube extends JPanel{
     int r;
     double c_squared; 
     Point3D cube; Point3D screen; Point3D eye; 
-    //int z_cube; int z_screen; int z_eye; 
+    Point3D[] perfect_real_points = new Point3D[8];
     Point3D[] real_points = new Point3D[8]; // one; Point two; Point three; Point four; 
     Point[] screen_points= new Point[8];
     Timer timer; int seconds = 0; 
@@ -21,27 +21,36 @@ public class Cube extends JPanel{
         cube = new Point3D(0,0,0);
         screen = new Point3D(0,0,70);
         eye = new Point3D(0,0,120); 
-        generateRealPoints(); 
+
+        generatePerfectRealPoints(); 
         
-        for(int i =6; i >= 0; i--){
+        for(int i =0; i < 7; i++){
+            //goes from 1 to 7 inclusive 
             connectionsList[i] = createConnectionList(i+1); 
             for(int j = 0; j < 3; j++){
-                System.out.print(connectionsList[i][j] + " ");
+                //System.out.print(connectionsList[i][j] + " ");
             }
-            System.out.println();
+            //System.out.println();
         }
+
         timer = new Timer(30, new TimerListener());
         timer.start();
     } 
 
-    public void generateRealPoints(){ 
+    // this is where i want to do the binary shift 
+    public void generatePerfectRealPoints(){ 
         //starts with a "1" and then adds in 0s 
         for(int i = 1; i <= 8; i++){
             int x = (i < 5) ? -r : r; 
             int y = (i%4 == 1 || i%4 == 2) ? -r : r; 
-            int z = (i%2 == 0) ? -r : r; 
+            //i have to add a z value because my cube is no
+            //longer at position (0,0,0) its at 
+            // (0,0,z)
+            int z = (i%2 == 0) ? -(cube.z +r) : (cube.z+r); 
+
             //minus 1 bc we go to index 8 
             real_points[i-1] = new Point3D(x,y,z); 
+            perfect_real_points[i-1] = new Point3D(x, y, z); 
             //System.out.println(x + ", " + y + ", " + z); 
         }
     }
@@ -51,58 +60,52 @@ public class Cube extends JPanel{
         public void actionPerformed(ActionEvent e) {
             seconds++; 
             seconds %= 360;
-            generateScreenPoints();
-            //xz_spin();
+            xz_spin();
         }
     }
 
+    //problem is there is a small lag (not continuous in rotation)
     public void xz_spin(){
         //we get the angle and then we add pi/4 because thats
         //where the first point is 
         //once you know the position of one point you have the rest
-        double degrees = seconds * (2*3.14/360) + 45; 
-        Point tr = new Point ((int) (Math.sqrt(c_squared)*Math.cos(degrees)), (int) (Math.sqrt(c_squared)*Math.sin(degrees))); 
-
-        degrees += 45;
-        Point br = new Point ((int) (Math.sqrt(c_squared)*Math.cos(degrees)), (int) (Math.sqrt(c_squared)*Math.sin(degrees))); 
-
+        double radians = (seconds/100.0);// % (2*Math.PI); 
 
         //only spins on the xz plane therefore we 
         //don't need to update the y value 
         for(int i = 0; i < 8; i++){
-            real_points[i].x = 12; 
-            real_points[i].z = 12; 
+            //angle in radians
+            //this is according to matrix mult 
+            real_points[i].z = (int) (perfect_real_points[i].z*Math.cos(radians)
+                - perfect_real_points[i].x*Math.sin(radians)); 
+
+            real_points[i].x = (int) (perfect_real_points[i].z*Math.sin(radians)
+                + perfect_real_points[i].x*Math.cos(radians)); 
+
+            System.out.println("x:" + real_points[i].x +
+                " z:" + real_points[i].z 
+            ); 
         }
+
+        System.out.println(); 
+
+        //this then converts points to 2d and then displays
+        transform(); 
 
     }
 
-    //where the real math occurs
-    public void generateScreenPoints(){
-        //the z_screen pt isn't super accurate 
-        //seconds = degrees
-
-        //generateReadPoints()
-        double radians = seconds * (2*3.14/360); 
-
-        //cycles through all the points 
+    // this is like project onto the screen 
+    public void transform(){
         for(int i =0; i< 8; i++){
-            double z_s = screen.z - real_points[i].z; 
-            double z_e = eye.z - real_points[i].z; 
-            double constant = z_s/z_e; 
-            //System.out.println("z_s:" + z_s + " z_e:" +z_e + " constant:" + constant);
-            //the minus is perspective of eye 
-            int temp_x = (int) ((real_points[i].x-Math.cos(radians)*120)*constant); 
+            double constant = (eye.z -screen.z - 0.0)/(eye.z - real_points[i].z);
+            //double constant = (real_points[i].z + 0.0)/screen.z; 
+            int temp_x = (int) ((real_points[i].x)*constant); 
             int temp_y = (int) ((real_points[i].y)*constant);  
             screen_points[i] = new Point(temp_x,temp_y);
-            
-            //System.out.print("x:" + temp_x); 
-            //System.out.print("y:" + temp_y + " ");
         }
-        //System.out.println();
-        repaint();
-        
+        repaint(); 
     }
-    
+ 
 
      private int[] createConnectionList(int decimal){
         int[] list = {-1,-1,-1}; 
@@ -140,11 +143,11 @@ public class Cube extends JPanel{
             for(int j = 0; j < 3; j++){
                 if(connectionsList[i-1][j] != -1){
                     int index = connectionsList[i-1][j];
-                    System.out.println("point1:" + i + " point2:" + index);
+                    //System.out.println("point1:" + i + " point2:" + index);
                     pen.drawLine(screen_points[i].x+offset, screen_points[i].y+offset, screen_points[index].x+offset, screen_points[index].y+offset);
                 }
             }
-            System.out.println(); 
+            //System.out.println(); 
         }
          
         pen.dispose();
